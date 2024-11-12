32ae6281e109c08930b5ec55ade6c34f7270c781
YUMI <zbrunnA.barreto@gmail.com>
2024-11-12 10:31:56 -0300

    Aula1
    print, variavel, input

---
+++ Aula 1/print.py
@@ -0,0 +1,30 @@
+#AULA 1 INTRODUÇAO E VARIAVEL
+
+#COMENTARIO
+
+"""
+DOCSTRING - ANOTAÇÔES
+"""
+
+#PRINT - ESCREVA 
+print("GOOD DIA!!!")
+print("Bruno Barreto de Almeida")
+# CRLF - CARACTERES QUE  RESPRESENTAM A QUEBRA DE LINHA DO SISTEMA 
+# \r - RETURN
+# \n - LINE FEED
+
+# END (end - fim do print)
+print('Uma informação','importante',end='\n')
+#sep (sep - separador do print)
+print('Uma informação','importante',sep= '----')
+print(123)
+
+#type
+print(type(11)) #int (inteiro) ex 1,0
+print(type(1.5)) #float (numero real) ex 1.5 -1.4 
+print(type('oi')) #str (caractere) ex "oi" "opa"
+print(type(True)) #bool (logico) ex true false
+
+#variavel
+teste = 'TESTE'
+print(teste,teste,teste)
