cc9d57e2934554dbfb59b01b912b2a5273b71cb7
YUMI <zbrunnA.barreto@gmail.com>
2024-11-12 11:43:31 -0300

    exercicios aula 1
    variavel, type e int

---
+++ Aula 1/Execicios/exercicios aula 1.py
@@ -0,0 +1,6 @@
+nome = input (print("DIGITE SEU NOME: "))
+email = input (print("DIGITE SEU EMAILS: "))
+senha = input (print("DIGITE SUA SENHA: "))
+
+verificando = nome, email, senha 
+print("\nSalvo com sucesso", nome,email, senha, sep= '\n')
---
+++ Aula 1/Execicios/exercicios2 aula 1.py
@@ -0,0 +1,3 @@
+ano = input("Informe o ano que voce nasceu: ")
+idae = int(2024) - int(ano)
+print("Valor", idae)
