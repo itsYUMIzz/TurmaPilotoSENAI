3123e95c75b5335b220588c02de50d5ca97d3b32
itsYUMIzz <zbrunnA.barreto@gmail.com>
2024-11-28 10:38:43 -0300

    finaly exept try
    e td q a de bom

---
+++ aula 9/atividade1/2/3/9.py
@@ -0,0 +1,4 @@
+for i in range(1,11):
+    print(i*8)
+    print(f"{i} x 8 = {i*8}")
+    
---
+++ aula 9/atividade1/2/9.py
@@ -0,0 +1,6 @@
+usuario = input("DIGITE O USUARIO: ")
+senha = input("DIGITE A SENHA: ")
+
+print("💖USUARIO ENCONTRADO💖" if usuario == "ADM" and senha == "123" else "💢FALHA💢")
+
+print("✨DEU TUDO CERTO✨" if usuario == "ADM" and senha == "123" else "💢FALHA💢")
